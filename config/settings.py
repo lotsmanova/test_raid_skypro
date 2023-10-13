9c5d683f2ddfbddd5f86dfde2b7e573a6773b536
violetta <lotsmanovavioletta@gmail.com>
2023-10-13 19:40:20 +0300

    add docs and urls

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_yasg',
 
 
     'main.apps.MainConfig',
@@ -131,6 +132,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 CORS_ALLOWED_ORIGINS = [
     "https://read-only.example.com",
