2983cd86888d8750952141e69d3145ed5b8e61d2
violetta <lotsmanovavioletta@gmail.com>
2023-10-13 19:22:58 +0300

    add model and fixture data

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class FrameworkModel(models.Model):
+
+    name = models.CharField(max_length=200, verbose_name='название')
+    language = models.CharField(max_length=200, verbose_name='язык программирования')
+
+    def __str__(self):
+        return f'{self.name} - {self.language}'
+
+    class Meta:
+        verbose_name = 'фреймворк'
+        verbose_name_plural = 'фреймворки'
