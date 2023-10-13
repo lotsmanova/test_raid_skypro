7219f0a3553784384dd6c55e2be8cc58cd75c30c
violetta <lotsmanovavioletta@gmail.com>
2023-10-13 19:53:31 +0300

    add filter framework

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView
 
 from main.models import FrameworkModel
@@ -7,3 +9,5 @@ from main.serializers import FrameworkListSerializer
 class FrameworkListAPIView(ListAPIView):
     queryset = FrameworkModel.objects.all()
     serializer_class = FrameworkListSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('language', )
